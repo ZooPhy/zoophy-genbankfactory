ded5a17eb1b94a6c31e382ff6001fc7e77ab6184
DeveloperDemetri <developerdemetri@gmail.com>
2017-03-14 15:15:50 -0700

    formatting some older files and regenerating java docs before merging

--- src/jp/ac/toyota_ti/coin/wipefinder/server/database/DBManager.java
+++ src/jp/ac/toyota_ti/coin/wipefinder/server/database/DBManager.java
@@ -1,6 +1,6 @@
 package jp.ac.toyota_ti.coin.wipefinder.server.database;
 
-import java.sql.Connection; 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -11,6 +11,7 @@ import jp.ac.toyota_ti.coin.wipefinder.server.utils.ResourceProvider.RP_PROVIDED
 
 /**
  * Manager with the connection to the DB
+ * 
  * @author Davy
  */
 public class DBManager {
@@ -19,92 +20,98 @@ public class DBManager {
 	private String Host = null;
 	private String User = null;
 	private String PW = null;
-	private Connection c = null; 
+	private Connection c = null;
+
 	/**
-	 * Create a Connection to the Database, where the details are provided by the properties provider open in the Dataprovider
+	 * Create a Connection to the Database, where the details are provided by
+	 * the properties provider open in the Dataprovider
+	 * 
 	 * @throws Exception
 	 */
 	public DBManager() throws Exception {
 		log.info("DB Interfacer initialisation...");
-		Name = (String)ResourceProvider.getPropertiesProvider(RP_PROVIDED_RESOURCES.PROPERTIES_PROVIDER).getValue("DB.Big.Name");
-		Host = (String)ResourceProvider.getPropertiesProvider(RP_PROVIDED_RESOURCES.PROPERTIES_PROVIDER).getValue("DB.Host");
-		User = (String)ResourceProvider.getPropertiesProvider(RP_PROVIDED_RESOURCES.PROPERTIES_PROVIDER).getValue("DB.User");
-		PW =   (String)ResourceProvider.getPropertiesProvider(RP_PROVIDED_RESOURCES.PROPERTIES_PROVIDER).getValue("DB.PW");
+		Name = (String) ResourceProvider.getPropertiesProvider(RP_PROVIDED_RESOURCES.PROPERTIES_PROVIDER)
+				.getValue("DB.Big.Name");
+		Host = (String) ResourceProvider.getPropertiesProvider(RP_PROVIDED_RESOURCES.PROPERTIES_PROVIDER)
+				.getValue("DB.Host");
+		User = (String) ResourceProvider.getPropertiesProvider(RP_PROVIDED_RESOURCES.PROPERTIES_PROVIDER)
+				.getValue("DB.User");
+		PW = (String) ResourceProvider.getPropertiesProvider(RP_PROVIDED_RESOURCES.PROPERTIES_PROVIDER)
+				.getValue("DB.PW");
 		try {
-			c = DriverManager.getConnection("jdbc:postgresql://"+Host+"/"+Name, User, PW);
+			c = DriverManager.getConnection("jdbc:postgresql://" + Host + "/" + Name, User, PW);
 			if (c != null) {
-				log.info("GenBank DB Connected: "+Name);
-			}
-			else {
+				log.info("GenBank DB Connected: " + Name);
+			} else {
 				log.log(Level.SEVERE, "Error occurred when connecting to the GenBank DB: null handler...");
-				throw new SQLException ("Error occurred when connecting to the GenBank DB: null handler...");
+				throw new SQLException("Error occurred when connecting to the GenBank DB: null handler...");
 			}
-		}
-		catch(SQLException se) {
-			log.log(Level.SEVERE, "Couldn't connect the postgres GenBank DB with options [jdbc:postgresql://"+Host+"/"+Name+" "+User+" "+PW+"]:"+se.getMessage());
+		} catch (SQLException se) {
+			log.log(Level.SEVERE, "Couldn't connect the postgres GenBank DB with options [jdbc:postgresql://" + Host
+					+ "/" + Name + " " + User + " " + PW + "]:" + se.getMessage());
 			throw se;
-		}
-		catch(Exception e) {
-			log.log(Level.SEVERE, "Other error occured when instantiate the GenBank DB:"+e.getMessage());
+		} catch (Exception e) {
+			log.log(Level.SEVERE, "Other error occured when instantiate the GenBank DB:" + e.getMessage());
 			throw e;
 		}
 	}
+
 	/**
 	 * Return the connection to the DB
+	 * 
 	 * @return The connection to an open DB
 	 */
 	public Connection getConnection() {
 		return c;
 	}
+
 	/**
 	 * Close the connection with the Database
-	 * @exception just an alert, we continue anyway
+	 * 
+	 * @exception just
+	 *                an alert, we continue anyway
 	 */
 	public void Close() {
 		try {
-			if(c!=null) {
-				log.info("Try to close the connection with the GenBank DB: "+Name);
+			if (c != null) {
+				log.info("Try to close the connection with the GenBank DB: " + Name);
 				c.close();
 				c = null;
 				log.info("Connection closed");
 			}
-		}
-		catch(SQLException se) {
-			log.log(Level.SEVERE, "Impossible to close the connection with the GenBank DB: "+se.getMessage());
+		} catch (SQLException se) {
+			log.log(Level.SEVERE, "Impossible to close the connection with the GenBank DB: " + se.getMessage());
 			return;
-		}
-		catch(Exception e) {
-			log.log(Level.SEVERE, "Other error when closing the GenBank DB:"+e.getMessage());
+		} catch (Exception e) {
+			log.log(Level.SEVERE, "Other error when closing the GenBank DB:" + e.getMessage());
 			return;
 		}
 	}
-	
+
 	public void connectToNewDB(String DBname) throws Exception {
-		if (c!=null) {
+		if (c != null) {
 			Close();
 			Name = DBname;
 			try {
-				//129.219.151.27
-				c = DriverManager.getConnection("jdbc:postgresql://"+Host+"/"+Name, User, PW);
+				// 129.219.151.27
+				c = DriverManager.getConnection("jdbc:postgresql://" + Host + "/" + Name, User, PW);
 				if (c != null) {
-					log.info("New connection to DB: "+Name);
-				}
-				else {
+					log.info("New connection to DB: " + Name);
+				} else {
 					log.log(Level.SEVERE, "Error occurred when connecting to the GenBank DB: null handler...");
-					throw new SQLException ("Error occurred when connecting to the GenBank DB: null handler...");
+					throw new SQLException("Error occurred when connecting to the GenBank DB: null handler...");
 				}
-			}
-			catch(SQLException se) {
-				log.log(Level.SEVERE, "Couldn't connect the postgres GenBank DB with options [jdbc:postgresql://"+Host+"/"+Name+" "+User+" "+PW+"]:"+se.getMessage());
+			} catch (SQLException se) {
+				log.log(Level.SEVERE, "Couldn't connect the postgres GenBank DB with options [jdbc:postgresql://" + Host
+						+ "/" + Name + " " + User + " " + PW + "]:" + se.getMessage());
 				throw se;
-			}
-			catch(Exception e) {
-				log.log(Level.SEVERE, "Other error occured when instantiate the GenBank DB:"+e.getMessage());
+			} catch (Exception e) {
+				log.log(Level.SEVERE, "Other error occured when instantiate the GenBank DB:" + e.getMessage());
 				throw e;
 			}
-		}
-		else {
+		} else {
 			throw new Exception("ERROR! Was not connected to DB to begin with...");
 		}
 	}
+	
 }
--- src/jp/ac/toyota_ti/coin/wipefinder/server/utils/PropertiesProvider.java
+++ src/jp/ac/toyota_ti/coin/wipefinder/server/utils/PropertiesProvider.java
@@ -17,125 +17,126 @@ import java.util.logging.Logger;
 
 /**
  * This object open the properties file and distribute the properties on request
+ * 
  * @author dw
  *
  */
 public class PropertiesProvider {
-	
-    private static final Logger log = Logger.getLogger("PropertiesProvider");
+
+	private static final Logger log = Logger.getLogger("PropertiesProvider");
 	private Properties table;
-	
+
 	/**
 	 * The local file containing the properties, default value local.properties
 	 */
 	private String properties_file = "local.properties";
 
-
 	/**
-	 * Read a file which contains the properties. Other values maybe include in the constructor.
+	 * Read a file which contains the properties. Other values maybe include in
+	 * the constructor.
+	 * 
 	 * @throws Exception
 	 */
-	public PropertiesProvider() throws Exception
-	{
-		log.info("PropertiesProvider initializing [File:"+properties_file+"]...");
+	public PropertiesProvider() throws Exception {
+		log.info("PropertiesProvider initializing [File:" + properties_file + "]...");
 		this.table = new Properties();
-		// Add any additional values required		
+		// Add any additional values required
 		this.loadProperties();
-		log.info("PropertiesProvider initialized [File:"+properties_file+"]...");
-	}
-	/**
-	 * Read a file which contains the properties. Other values maybe include in the constructor.
-	 * @param namePropertiesFile String Name of the properties File
-	 * @throws Exception
-	 */
-	public PropertiesProvider(String namePropertiesFile) throws Exception
-	{
-		log.info("PropertiesProvider initializing [File:"+namePropertiesFile+"]...");
-		properties_file = namePropertiesFile;
-		if(this.table==null)
-			this.table = new Properties();
-		// Add any additional values required		
-		this.loadProperties();
-		log.info("PropertiesProvider initialized [File:"+namePropertiesFile+"].");
+		log.info("PropertiesProvider initialized [File:" + properties_file + "]...");
 	}
 
 	/**
-	 * Load the properties file in memory.
-	 * The File is closed when loading is done.
+	 * Read a file which contains the properties. Other values maybe include in
+	 * the constructor.
+	 * 
+	 * @param namePropertiesFile
+	 *            String Name of the properties File
 	 * @throws Exception
 	 */
-	private void loadProperties() throws Exception
-	{
-		InputStream is=null;
-		try
-		{
-			//we check if the properties_file as a valid path first
+	public PropertiesProvider(String namePropertiesFile) throws Exception {
+		log.info("PropertiesProvider initializing [File:" + namePropertiesFile + "]...");
+		properties_file = namePropertiesFile;
+		if (this.table == null)
+			this.table = new Properties();
+		// Add any additional values required
+		this.loadProperties();
+		log.info("PropertiesProvider initialized [File:" + namePropertiesFile + "].");
+	}
+
+	/**
+	 * Load the properties file in memory. The File is closed when loading is
+	 * done.
+	 * 
+	 * @throws Exception
+	 */
+	private void loadProperties() throws Exception {
+		InputStream is = null;
+		try {
+			// we check if the properties_file as a valid path first
 			File checkedPath = new File(this.properties_file);
-			if(checkedPath.exists() && checkedPath.canRead())
+			if (checkedPath.exists() && checkedPath.canRead())
 				is = new FileInputStream(new File(this.properties_file));
-			else //we search the file according to the location of the .class				
+			else // we search the file according to the location of the .class
 				is = this.getClass().getClassLoader().getResourceAsStream(this.properties_file);
-			 
+
 			this.table.load(is);
-			log.info("Properties File ["+properties_file+"] was loaded successfully");			
-		}catch(Exception e){
-			log.log(Level.SEVERE, "Properties File ["+properties_file+"] was not loaded properly: " + e.getMessage());
+			log.info("Properties File [" + properties_file + "] was loaded successfully");
+		} catch (Exception e) {
+			log.log(Level.SEVERE,
+					"Properties File [" + properties_file + "] was not loaded properly: " + e.getMessage());
 			throw e;
-		}finally
-		{
-			try
-			{
+		} finally {
+			try {
 				is.close();
-			}catch(IOException e)
-			{
-				log.log(Level.SEVERE, "Impossible to close the properties file ["+properties_file+"]:"+e.getMessage());
+			} catch (IOException e) {
+				log.log(Level.SEVERE,
+						"Impossible to close the properties file [" + properties_file + "]:" + e.getMessage());
 				return;
-			}	
+			}
 		}
 	}
-	
-	public Object getValue(String property){
-		if(this.table.containsKey(property)){
+
+	public Object getValue(String property) {
+		if (this.table.containsKey(property)) {
 			return this.table.getProperty(property);
-		}else{
-			log.warning("The property value ["+property+"] asked in the properties file ["+properties_file+"] is missing.");
+		} else {
+			log.warning("The property value [" + property + "] asked in the properties file [" + properties_file
+					+ "] is missing.");
 			return null;
 		}
 	}
-	
-	public void setValue(String property, Object value)
-	{
+
+	public void setValue(String property, Object value) {
 		this.table.put(property, value);
 	}
-	
+
 	/**
-	 * Merge this Properties Provider with the one given in Parameter, 
-	 * if a property already exits the old value is kept
+	 * Merge this Properties Provider with the one given in Parameter, if a
+	 * property already exits the old value is kept
 	 */
-	public void mergeWith(PropertiesProvider pp)
-	{
+	public void mergeWith(PropertiesProvider pp) {
 		Properties newsProperties = pp.getTable();
 		Enumeration<?> keys = newsProperties.propertyNames();
-		while(keys.hasMoreElements())
-		{
+		while (keys.hasMoreElements()) {
 			Object elementKey = keys.nextElement();
-			//check if the elementKey exist in the current Properties Provider
-			if(this.table.containsKey(elementKey))
-			{
-				log.warning("The property ["+elementKey.toString()+"->"+pp.getValue((String)elementKey)+"] already exist in the Property Provider, the old value ["+getValue((String)elementKey)+"] is kept.");
-			}
-			else
-			{
-				this.table.put(elementKey, pp.getValue((String)elementKey));
+			// check if the elementKey exist in the current Properties Provider
+			if (this.table.containsKey(elementKey)) {
+				log.warning("The property [" + elementKey.toString() + "->" + pp.getValue((String) elementKey)
+						+ "] already exist in the Property Provider, the old value [" + getValue((String) elementKey)
+						+ "] is kept.");
+			} else {
+				this.table.put(elementKey, pp.getValue((String) elementKey));
 			}
 		}
 	}
-	protected Properties getTable()
-	{
+
+	protected Properties getTable() {
 		return this.table;
 	}
-	public void Close(){
+
+	public void Close() {
 		table.clear();
 		table = null;
 	}
+	
 }
